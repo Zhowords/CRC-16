c9e33f2b33a1b28e023ea6d1e129208a00547362
zhowords <zhowords@Outlook.com>
2017-11-24 23:06:38 +0800

    循坏冗余检测

---
+++ CRC-16/CRC-16/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace CRC_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBox_M = new System.Windows.Forms.TextBox();
+            this.txtBox_P = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(138, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入M：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(90, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "请输入除数P：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(105, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(105, 182);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(105, 235);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 24);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(105, 285);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 24);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "label6";
+            // 
+            // txtBox_M
+            // 
+            this.txtBox_M.Location = new System.Drawing.Point(246, 12);
+            this.txtBox_M.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBox_M.Name = "txtBox_M";
+            this.txtBox_M.Size = new System.Drawing.Size(272, 34);
+            this.txtBox_M.TabIndex = 6;
+            // 
+            // txtBox_P
+            // 
+            this.txtBox_P.Location = new System.Drawing.Point(246, 54);
+            this.txtBox_P.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtBox_P.Name = "txtBox_P";
+            this.txtBox_P.Size = new System.Drawing.Size(272, 34);
+            this.txtBox_P.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(246, 285);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(281, 79);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "执行CRC与FCS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(661, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(366, 510);
+            this.listBox1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(246, 407);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(281, 79);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "清空ListBox";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1120, 579);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBox_P);
+            this.Controls.Add(this.txtBox_M);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBox_M;
+        private System.Windows.Forms.TextBox txtBox_P;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CRC-16/CRC-16/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRC_16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+         
+        //储存数据
+        char[] char_M;
+        //储存（数据+冗余码）
+        char[] char_m;
+        //储存除数
+        char[] char_P;
+        //储存余数
+        char[] char_R;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string M = txtBox_M.Text;
+            string P = txtBox_P.Text;
+            char_M = M.ToCharArray();
+            char_P = P.ToCharArray();
+            string _2nM = "被除数2^nM=" + M + "000";
+            //label3.Text = _2nM;
+
+            char_R = new char[char_P.Length - 1];
+            CRC();
+            FCS();
+        }
+        void CRC()
+        {
+            //储存被除数是 2nM  数组长度是k+n
+            char_m = new char[(char_M.Length + char_P.Length - 1)];
+
+            for (int i = 0; i < char_M.Length; i++)
+            {
+                char_m[i] = char_M[i];
+            }
+            //补零
+            for (int i = char_M.Length; i < (char_M.Length+char_P.Length-1); i++)
+            {
+                char_m[i] = '0';
+            }
+            
+            string m = null; ;
+            for (int i = 0; i < (char_M.Length+char_P.Length-1); i++)
+            {
+                m += char_m[i];//将数组变成string类型 方便输出（被除数是 2nM）
+               
+            }
+            listBox1.Items.Add("CRC:");
+            listBox1.Items.Add(m);
+            Mode2();
+        }
+        void FCS()
+        {
+            for (int i = 0; i < char_M.Length; i++)
+            {
+                char_m[i] = char_M[i];
+                
+            }
+            for (int i = char_M.Length; i < char_m.Length; i++)
+            {
+                char_m[i] = char_R[i - char_M.Length];
+            }
+
+            string m = null;
+            for (int i = 0; i < char_m.Length; i++)
+            {
+                m += char_m[i];
+            }
+            listBox1.Items.Add("FCS:");
+            listBox1.Items.Add(m);
+            Mode2();
+        }
+        void Mode2()
+        {
+            //循坏 被除数
+            for (int i = (char_P.Length-1); i < (char_M.Length+char_P.Length-1); i++)
+            {
+                //储存被除数
+                char[] f = new char[char_P.Length];
+
+                //当所取被除数的前四位的第一位为零时，除数取零
+                char[] q = new char[char_P.Length];
+                //第一次取被除数的前四位
+                if (i<char_P.Length)
+                {
+                    for (int j = 0; j < char_P.Length; j++)
+                    {
+                        f[j] = char_m[j];
+                    }
+                }
+                else
+                {   
+                    // 第二次以后是 取余数加被除数的5位 构成新被除数的前四位
+                    for (int j = 0; j < char_P.Length-1; j++)
+                    {
+                        f[j] = char_R[j];
+                    }
+                    f[char_P.Length-1] = char_m[i];
+                }
+
+                listBox1.Items.Add("char_m[" + i + "]:" + char_m[i]);
+
+
+                string r=null;
+                for (int j = 0; j < char_P.Length; j++)
+                {
+                    r += f[j];
+                }
+                listBox1.Items.Add("此时被除数f[]:" + r);
+                //除数 若当所取被除数的前四位的第一位不为零时，除数取输入值  
+                if (f[0]!='0')
+                {
+                    for (int j = 0; j < char_P.Length; j++)
+                    {
+                        q[j] = char_P[j];
+                    }
+                    
+                }
+                else
+                {
+                    for (int j = 0; j < char_P.Length; j++)
+                    {
+                        q[j] = '0';
+                    }
+                }
+                //输出除数（strq）
+                string strq=null;
+                for (int j = 0; j < char_P.Length; j++)
+                {
+                    strq += q[j];
+                }
+                listBox1.Items.Add("此时除数q[]:" + strq);
+                //获取余数
+                for (int j =1 ; j < char_P.Length; j++)
+                {
+                    if ((int)f[j] + (int)q[j]-96 == 0 || (int)f[j] + (int)q[j]-96 == 2)
+                    {
+                        char_R[j - 1] = '0';
+                    }
+                    else {
+                        char_R[j - 1] = '1';
+                    }
+                }
+
+                //输出余数
+                string strR = null;
+                for (int j = 0; j < char_P.Length-1; j++)
+                {
+                    strR += char_R[j];
+                }
+                listBox1.Items.Add("此时余数char_R[]:"+strR);
+                listBox1.Items.Add("");
+         }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           listBox1.Items.Clear();
+        }
+             
+    }
+}
